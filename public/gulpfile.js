77b9a2724c244d02eaf6710afe4081b60a263f57
Ivan Kaduk <ceditvodu@gmail.com>
2016-07-20 09:35:55 +0300

    change some structure

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -3,12 +3,12 @@ var uglifyjs = require('gulp-uglifyjs');
 var concat = require('gulp-concat');
 
 gulp.task('default', function() {
-  gulp.src('../*.js')
+  gulp.src('../app/*.js')
   .pipe(uglifyjs('app.min.js', {
       outSourceMap: true
     }))
   .pipe(gulp.dest('app/'));
-    gulp.src('../*.js')
+    gulp.src('../app/*.js')
         .pipe(concat('jdoc.js'))
         .pipe(gulp.dest('app/'));
   
